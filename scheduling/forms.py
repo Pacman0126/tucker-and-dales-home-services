65f3d21c58da416d363625aff728f52e7094b017
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-10-01 16:10:43 +0200

    Refactor forms to use DB-driven ModelChoiceFields for ServiceCategory and TimeSlot

--- scheduling/forms.py
+++ scheduling/forms.py
@@ -1,12 +1,5 @@
 from django import forms
-from .models import ServiceCategory
-
-TIME_SLOTS = [
-    ("7:30-9:30", "7:30 – 9:30"),
-    ("10:00-12:00", "10:00 – 12:00"),
-    ("12:30-2:30", "12:30 – 2:30"),
-    ("3:00-5:00", "3:00 – 5:00"),
-]
+from .models import ServiceCategory, TimeSlot
 
 
 class SearchByDateForm(forms.Form):
@@ -29,9 +22,10 @@ class SearchByDateForm(forms.Form):
 
 
 class SearchByTimeSlotForm(forms.Form):
-    time_slot = forms.ChoiceField(
-        choices=TIME_SLOTS,
+    time_slot = forms.ModelChoiceField(
+        queryset=TimeSlot.objects.all(),
         widget=forms.Select(attrs={"class": "form-select"}),
+        empty_label="Select a time slot",
     )
     service_category = forms.ModelChoiceField(
         queryset=ServiceCategory.objects.all(),
