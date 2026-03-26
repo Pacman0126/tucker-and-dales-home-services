129bbec2ceb79ece6839b661e6bc78029ef04112
Pacman0126 <oliver.p.hartmann@gmail.com>
2026-03-26 21:41:50 +0100

    Degug favicon related 500 error on Heroku deployment

--- tucker_and_dales_home_services/urls.py
+++ tucker_and_dales_home_services/urls.py
@@ -1,24 +1,9 @@
 """
 URL configuration for tucker_and_dales_home_services project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.shortcuts import render
-from django.templatetags.static import static
 from django.urls import include, path
-from django.views.generic import RedirectView
 
 
 def test_500(request):
@@ -33,10 +18,6 @@ handler404 = "core.views.custom_404"
 handler500 = "core.views.custom_500"
 
 urlpatterns = [
-    path(
-        "favicon.ico",
-        RedirectView.as_view(url=static("images/favicon.ico"), permanent=True),
-    ),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include(("core.urls", "core"), namespace="core")),
@@ -50,6 +31,6 @@ urlpatterns = [
         include(("scheduling.urls", "scheduling"), namespace="scheduling"),
     ),
 
-    # # TEMPORARY: remove after testing custom 500 handling
-    # path("test-500/", test_500, name="test_500"),
+    # TEMPORARY: remove after testing custom 500 handling
+    path("test-500/", test_500, name="test_500"),
 ]
